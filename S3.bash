8f3975110edc2a74a2ddb6b58f1f57972be64ae1
Gabriel F <43410045+GabrielFGRL@users.noreply.github.com>
2019-05-03 13:51:52 +0200

    Add files via upload

--- S3.bash
+++ S3.bash
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "rentrez l'URL"
-read url
-youtube-dl $url
+
+video() {
+    echo "rentrez l'URL"
+    read url
+    youtube-dl -F137 $url
+}
+
+musique() {
+    echo "rentrez l'URL"
+    read url
+    youtube-dl -F140 $url
+}
+
+#choix video ou musique puis choix qualité
+echo "veux tu la vidéo [y/n]"
+read choix
+if [[ "$choix" -eq "y" ]]; then
+    video
+    exit 1
+else
+    musique
+    exit 1
+fi
 
 exit 0
 
